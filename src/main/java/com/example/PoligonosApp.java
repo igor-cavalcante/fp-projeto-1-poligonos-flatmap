beee96e1dd690496184c569c8431e2ad759de959
igor-cavalcante <igorcavalcantealbuquerque@gmail.com>
2025-02-25 09:40:52 -0300

    implementando função que identifica o tipo de cada poligono

--- src/main/java/com/example/PoligonosApp.java
+++ src/main/java/com/example/PoligonosApp.java
@@ -130,9 +130,17 @@ public class PoligonosApp extends Application {
      * @return uma lista de String indicando se o polígono é um "quadrilátero" (quadrado ou retângulo),
      * "triângulo", "pentágono", "hexágono" ou apenas um "polígono" geral quando tiver mais de 6 lados.
      */
-    protected List<String> tipoPoligonos(){
-        // TODO Apague esta linha e a próxima e implemente seu código
-        return List.of();
+    protected List<String> tipoPoligonos() {
+        return pontosPoligonos.stream()
+                .map(List::size)
+                .map(sides -> switch (sides) {
+                    case 3 -> "Triângulo";
+                    case 4 -> "Quadrilátero";
+                    case 5 -> "Pentágono";
+                    case 6 -> "Hexágono";
+                    default -> "Polígono de " + sides + " lados";
+                })
+                .toList();
     }
 
     /**
